7b9b9519341112991a6954d70d6cfe3ef46d5552
Miguel Quispe <dev@miguelquispe.com>
2023-02-06 15:47:51 -0500

    chore: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { GTMscript } from '@components/GTM';
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
-import Script from 'next/script';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GTMscript />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
