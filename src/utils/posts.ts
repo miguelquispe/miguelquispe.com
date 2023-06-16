60c79e9d2f86ede5b5b0890532da5cab3c9a6eb4
Miguel Quispe <dev@miguelquispe.com>
2023-06-16 15:07:44 -0500

    chore: add mdx support for footnotes

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -10,6 +10,8 @@ import rehypeSlug from 'rehype-slug';
 import rehypeCodeTitles from 'rehype-code-titles';
 
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkGfm from 'remark-gfm';
+import remarkRehype from 'remark-rehype';
 
 const root = process.cwd();
 
@@ -60,6 +62,10 @@ export async function getPostFromSlug(slug: string): Promise<Post> {
 
   const mdxSource = await serialize(content, {
     mdxOptions: {
+      remarkPlugins: [
+        remarkGfm,
+        [remarkRehype, { footnoteLabel: 'Notas', footnoteLabelTagName: 'h4' }],
+      ],
       rehypePlugins: [
         rehypeSlug,
         rehypeCodeTitles,
