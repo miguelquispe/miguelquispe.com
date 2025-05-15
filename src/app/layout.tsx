76ec95dde08347a5c169757a203c964995dd9ddc
Miguel Quispe <dev@miguelquispe.com>
2025-05-15 08:20:03 -0500

    refactor: add vercel scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import { GTMnoscript, GTMscript } from '@components/GTM';
+import { Analytics } from '@vercel/analytics/react';
 
 // Custom Fonts
 const inter = Inter({
@@ -78,6 +79,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <main>{children}</main>
         <Footer />
       </body>
+      <Analytics />
     </html>
   );
 }
