5b1eae3ece7b4320cb9dc1a9d7f18f1312a32766
Miguel Quispe <dev@miguelquispe.com>
2025-06-18 06:24:36 -0500

    fix: add correct analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 import { GTMnoscript, GTMscript } from '@components/GTM';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 
 // Custom Fonts
 const inter = Inter({
